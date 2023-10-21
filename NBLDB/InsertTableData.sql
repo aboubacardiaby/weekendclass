53116af7e4374fe2468604597e1bc25a6c3560e5
Abou Diaby <adiaby@owens-minor.com>
2023-10-21 11:37:54 -0500

    add inserdata

---
+++ NBLDB/InsertTableData.sql
@@ -0,0 +1,17 @@
+﻿USE [NBLDB]
+GO
+DELETE [dbo].[tblCustomer]
+INSERT INTO [dbo].[tblCustomer]
+           ([CustId]
+           ,[FirstName]
+           ,[LastName]
+           ,[Email]
+           ,[PhoneNumber]
+           ,[Address]
+           ,[City]
+           ,[State]
+           )
+  VALUES ('002','David','Johnson','david.johnson@gmail.com','612-239-6000','1 first street','Minneapolis','MN')
+GO
+
+
